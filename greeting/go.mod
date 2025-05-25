bd26cd093e98604f4cd1c6a743a5781e2bd45bc1
Ilhomjon <futurewithme1221@gmail.com>
2025-05-25 23:20:56 +0500

    chore: Worked on Go array syntax

--- greeting/go.mod
+++
@@ -1,3 +0,0 @@
-module greeting/greeting
-
-go 1.24.3
--- main.go
+++ main.go
@@ -1,12 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
-func main() {
-	var myVar int = 10
-	fmt.Println(myVar)
+type UserInfo struct {
+	Name     string
+	LastName string
+	Age      int
+}
 
-	fmt.Println(gree)
+func main() {
+	callUserINfo()
+	sliceArray()
+
+	reader := bufio.NewReader((os.Stdin))
+	fmt.Println("Enter your name:")
+	name, _ := reader.ReadString('\n')
+	fmt.Println(name)
+}
+
+func sliceArray() {
+	primes := [4]int{3, 4, 5, 6}
+
+	var s []int = primes[0:2]
+
+	fmt.Println(s)
+}
+
+func callUserINfo() {
+	users := []UserInfo{{Name: "John", LastName: "Doe", Age: 30}, {Name: "Jane", LastName: "Smith", Age: 25}}
+
+	for i := 0; i < len(users); i++ {
+		fmt.Println(users[i].Name)
+	}
+	fmt.Println(("User Information:"))
 }
